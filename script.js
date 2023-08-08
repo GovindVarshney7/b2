414aafba7105288a651ee3e1e9c9246af19a62f2
Govind Varshney <90921686+GovindVarshney7@users.noreply.github.com>
2023-08-08 19:07:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let obj={
+    color:{
+        red:12,
+        blue:30,
+        green:9
+    },
+    height:{
+        cm:100,
+        vw:50,
+    },
+    width:20,
+    type:"wooden",
+      
+
+}
